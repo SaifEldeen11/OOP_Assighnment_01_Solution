3d4127070e6359993ea248d30491745b9a88acb6
SaifAhmed11 <saifeldeenahmed942@gmail.com>
2025-07-05 13:19:32 +0300

    Add project files.

---
+++ OOP_Assighnment_01/Program.cs
@@ -0,0 +1,39 @@
+﻿using OOP_Assighnment_01;
+namespace OOP_Assighnment_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //foreach (object day in Enum.GetNames<WeekDays>())
+            //{
+            //    Console.WriteLine(day);
+            //}
+            #endregion
+
+            #region Q2
+            //Console.WriteLine("enter the month name ");
+            //string temp = Console.ReadLine() ?? "no month ";
+            //if (Enum.TryParse(temp, true, out Seas selectedSeason))
+            //{
+            //    string monthRange = selectedSeason switch
+            //    {
+            //        Seas.Spring => "March to May",
+            //        Seas.Summer => "June to August",
+            //        Seas.Autumn => "September to November",
+            //        Seas.Winter => "December to February",
+            //        _ => "Unknown season"
+            //    };
+
+            //    Console.WriteLine($"The months for {selectedSeason} are: {monthRange}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid season name.");
+            //}
+
+            #endregion
+        }
+    }
+}
---
+++ OOP_Assighnment_01/WeekDays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assighnment_01
+{
+    enum WeekDays
+    {
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+        sunday,
+    }
+    enum Seas
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
